c0fae1834bb4bc0ce59256ba88eaa574d641df6b
Victor Emanuel <victoremannuel12@hotmail.com>
2022-07-26 23:43:42 -0300

    finished the tabBar in tabs.js and incluid more api in package.json

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,51 +1,214 @@
 import React from "react";
-import { View,Image,TouchableOpacity } from "react-native";
-import { createBottomTabNavigator,BottomTabBar } from '@react-navigation/bottom-tabs'
-
+import { View, Image, TouchableOpacity } from "react-native";
+import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs'
+import Svg, { Path } from 'react-native-svg'
 import { Home } from "../screens";
-import {COLORS,icons} from '../constants'
+import { COLORS, icons } from '../constants'
+import { isIphoneX } from 'react-native-iphone-x-helper'
+import { Ionicons, AntDesign, Feather, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+
+
 const Tab = createBottomTabNavigator()
 
-const Tabs =()=>{
-    return(
-        <Tab.Navigator tabBarOptions={{showLabel:false}}>
-            <Tab.Screen 
+// component function to modify the bottom tabs when is selected 
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
+    var isSelected = accessibilityState.selected
+    if (isSelected) {
+        return (
+            <View style={{ flex: 1, alignItems: "center" }}>
+                <View style={{ flexDirection: 'row', position: 'absolute', top: 0 }}>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                    <Svg
+                        width={75}
+                        height={61}
+                        viewBox="0 0 75 61"
+
+                    >
+                        <Path
+                            d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+                            fill={COLORS.white}
+                        />
+                    </Svg>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        top: -22.5,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        backgroundColor: COLORS.white,
+
+                    }}
+                    onPress={onPress}
+                >
+                    {children}
+                </TouchableOpacity>
+            </View>
+
+        )
+    } else {
+        return (
+            <TouchableOpacity
+                style={{
+                    flex: 1,
+                    height: 60,
+                    backgroundColor: COLORS.white
+                }}
+                activeOpacity={1}
+                onPress={onPress}
+            >
+                {children}
+            </TouchableOpacity>
+        )
+    }
+}
+// component functiont that put space between the Iphone's tabBar by default and tabBar of my App
+const CustomTabBar = (props) => {
+    if (isIphoneX()) {
+        return (
+            <View>
+                <View
+                    style={{
+                        position: 'absolute',
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        height: 30,
+                        backgroundColor: COLORS.white
+                    }}
+                >
+
+                </View>
+                <BottomTabBar {...props.props} />
+            </View>
+        )
+    } else {
+        return <BottomTabBar {...props.props} />
+
+    }
+}
+
+const Tabs = () => {
+
+    return (
+        <Tab.Navigator
+            tabBarOptions={{
+                showLabel: false,
+
+
+            }}
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: "transparent",
+                    borderTopWiddth: 0,
+                    elevation: 0,
+                    height: 60,
+
+
+                },
+
+            }}
+            tabBar={(props) => (
+                <CustomTabBar props={props} />
+            )}
+        >
+            <Tab.Screen
                 name="Home"
                 component={Home}
                 options={{
-                    tabBarIcon:({focused}) => (
-                        <Image 
-                            source={icons.cutlery}
-                            resizeMode="contain"
-                            style={{
-                                width:25,
-                                height:25,
-                                tintColor: focused ? COLORS.primary :COLORS.secondary 
-                            }}
-                        />
-                    )
+                    tabBarIcon: ({ focused }) => (
+                        <MaterialCommunityIcons name="silverware-fork-knife" size={28} style={{ color: focused ? COLORS.primary : COLORS.secondary }} />
+
+                        //  modify of image in tab navigation ( module test) next step.
+
+                        // <Image
+                        //     source={icons.cutlery}
+                        //     resizeMode="contain"
+                        //     style={{
+                        //         width: 25,
+                        //         height: 25,
+                        //         tintColor: focused ? COLORS.primary : COLORS.secondary
+                        //     }}
+                        // />
+                    ),
+                    tabBarButton: props => <TabBarCustomButton {...props} />
                 }}
 
             />
-            <Tab.Screen 
-                name="Home"
+            <Tab.Screen
+                name="Search"
                 component={Home}
                 options={{
-                    tabBarIcon:({focused}) => (
-                        <Image 
-                            source={icons.cutlery}
-                            resizeMode="contain"
-                            style={{
-                                width:25,
-                                height:25,
-                                tintColor: focused ? COLORS.primary :COLORS.secondary 
-                            }}
-                        />
-                    )
+                    tabBarIcon: ({ focused }) => (
+                        <Feather name="search" size={28} style={{ color: focused ? COLORS.primary : COLORS.secondary }} />
+                        //  modify of image in tab navigation ( module test) next step.
+
+                        // <Image
+                        //     source={icons.search}
+                        //     resizeMode="contain"
+                        //     style={{
+                        //         width: 25,
+                        //         height: 25,
+                        //         tintColor: focused ? COLORS.primary : COLORS.secondary
+                        //     }}
+                        // />
+                    ),
+                    tabBarButton: props => <TabBarCustomButton {...props} />
+
                 }}
 
             />
-            
+            <Tab.Screen
+                name="Like"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <AntDesign name="heart" size={28} style={{ color: focused ? COLORS.primary : COLORS.secondary }} />
+                        //  modify of image in tab navigation ( module test) next step.
+
+                        // <Image
+                        //     source={icons.like}
+                        //     resizeMode="contain"
+                        //     style={{
+                        //         width: 25,
+                        //         height: 25,
+                        //         tintColor: focused ? COLORS.primary : COLORS.secondary
+                        //     }}
+                        // />
+                    ),
+                    tabBarButton: props => <TabBarCustomButton {...props} />
+
+                }}
+
+            />
+            <Tab.Screen
+                name="User"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons name="person-sharp" size={28} style={{ color: focused ? COLORS.primary : COLORS.secondary }} resizeMode="contain" />
+
+                        //  modify of image in tab navigation ( module test) next step.
+                        // <Image
+                        //     source={icons.user}
+                        //     resizeMode="contain"
+                        //     style={{
+                        //         width: 25,
+                        //         height: 25,
+                        //         tintColor: focused ? COLORS.primary : COLORS.secondary
+                        //     }}
+                        // />
+                    ),
+                    tabBarButton: props => <TabBarCustomButton {...props} />
+                }}
+
+            />
+
+
         </Tab.Navigator>
     )
 }
