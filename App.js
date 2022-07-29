b821fd7960c54299a74712bcfab1bb6a45f60e39
Victor Emanuel <victoremannuel12@hotmail.com>
2022-07-29 01:50:12 -0300

    Correction of ScreenOptions that was using old React Navigation component.

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Tabs from './navigation/tabs';
-// import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native'
 import { Home, Restaurant, OrderDelivery } from './screens'
@@ -10,19 +9,19 @@ import { Home, Restaurant, OrderDelivery } from './screens'
 export default function App() {
   const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
+    < NavigationContainer >
       <Stack.Navigator
         screenOptions={{
-          headerShown:false
+          headerShown: false
         }}
         initialRouteName={"Home"}
       >
-        <Stack.Screen name="Home" component={Tabs}/>
-        <Stack.Screen name="Restaurant" component={Restaurant}/>
-        <Stack.Screen name="OrderDelivery" component={OrderDelivery}/>
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
 
       </Stack.Navigator>
-    </NavigationContainer>
-  
+    </ NavigationContainer>
+
   );
 }
