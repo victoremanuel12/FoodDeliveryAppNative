6397566d405e752f477efaf164b66614e566037c
Victor Emanuel <victoremannuel12@hotmail.com>
2022-07-29 21:02:45 -0300

    Add a UseFonts Expo to aply on app

--- App.js
+++ App.js
@@ -4,10 +4,21 @@ import Tabs from './navigation/tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native'
 import { Home, Restaurant, OrderDelivery } from './screens'
+import { useFonts } from 'expo-font';
 
 
 export default function App() {
   const Stack = createStackNavigator();
+  const [loaded] = useFonts({
+    "Roboto-Black": require('./assets/fonts/Roboto-Black.ttf'),
+    "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'),
+    "Roboto-Regular": require('./assets/fonts/Roboto-Regular.ttf'),
+
+  })
+
+  if (!loaded) {
+    return null;
+  }
   return (
     < NavigationContainer >
       <Stack.Navigator
