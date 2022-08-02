baaae39c951998f2e3ebc149285bc7cf832488e6
Victor Emanuel <victoremannuel12@hotmail.com>
2022-08-01 21:43:24 -0300

    create  the  navigation of app  and share the Fonts

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { useFonts } from 'expo-font';
 
 export default function App() {
   const Stack = createStackNavigator();
+
   const [loaded] = useFonts({
     "Roboto-Black": require('./assets/fonts/Roboto-Black.ttf'),
     "Roboto-Bold": require('./assets/fonts/Roboto-Bold.ttf'),
